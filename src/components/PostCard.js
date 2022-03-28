dd896b9ba59c290df726fcc840bf0e7ed9977878
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-03-28 10:43:07 +0200

    updt

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,28 @@
+// import Checkbox from "../components/Checkbox";
+import { useNavigate } from "react-router-dom";
 export default function PostCard({ task }) {
   //prop er task, kunne kaldes alt mulig. skal matche med det data der hentes fra firebase.
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`posts/${task.id}`);
+  }
+
   return (
     <>
       <article>
-        <h2>{task.title}</h2>
-        <p>{task.body}</p>
-        <p>{task.list}</p>
-        <p>{task.share}</p>
+        <div className="PostCard_cntr">
+          <div className="checkbox_div">
+            {/* <Checkbox /> */}
+            <div className="todo-text">
+              <h3>{task.title}</h3>
+              <div className="update">
+                <button onClick={handleClick}>update</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </article>
     </>
   );
