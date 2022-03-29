5e6a45f72b92790392fbf0f3e06184ca9bece344
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-03-29 15:04:31 +0200

    minus icon

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { getAuth, signOut, deleteUser, EmailAuthProvider } from "firebase/auth";
 import { doc, getDoc, setDoc } from "@firebase/firestore";
 import placerholder from "../assets/profile-picture.jpg";
+import { HiMinusCircle } from "react-icons/hi";
 
 
 export default function ProfilePage({ currentUser }) {
@@ -110,10 +111,10 @@ export default function ProfilePage({ currentUser }) {
                             <input type="text" value={user?.name} onChange={handleChange} name="name" placeholder="Navn" />
                         {/* <label for="email">Email</label> */}
                             <input type="email" value={user?.email} onChange={handleChange} name="email" placeholder="bruger@mail.dk" />
-                        <button>Gem</button>
+                        <button className="btn">Gem</button>
 
                         <div className="profile-btn-cntr">
-                           <button className="" onClick={handleSignOut} data-id={user.id}>Log ud</button>
+                           <button className="btn" onClick={handleSignOut} data-id={user.id}>Log ud</button>
                             <button className="btn-outline" onClick={handleUserDelete} data-id={user.id}>Slet bruger</button>
                         </div>
                     </form>
@@ -132,7 +133,7 @@ export default function ProfilePage({ currentUser }) {
                                 <input type="text" className="group-member" value={user?.name} name="name" placeholder={`${user?.name}`} />
                                 <input type="email" className="group-member" value={user?.email} name="email" placeholder={`${user?.email}`} />
                             </div>
-                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}>-</button>
+                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}><HiMinusCircle /></button>
                         </div>
 
                         <div className="group-members-box">
@@ -143,8 +144,7 @@ export default function ProfilePage({ currentUser }) {
                                 <input type="text" className="group-member" value={user?.name} name="name" placeholder="Gruppemedlem" />
                                 <input type="email" className="group-member" value={user?.email} name="email" placeholder="medlem@email.dk" />
                             </div>
-                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}>-</button> {/* OBS - pt slet user knap */}
-
+                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}> <HiMinusCircle /> </button> {/* OBS - pt slet user knap */}
                         </div>
 
                         <div className="group-members-box">
@@ -155,7 +155,7 @@ export default function ProfilePage({ currentUser }) {
                                 <input type="text" className="group-member" value={user?.name} name="name" placeholder="Gruppemedlem" />
                                 <input type="email" className="group-member" value={user?.email} name="email" placeholder="medlem@email.dk" />
                             </div>
-                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}>-</button>
+                            <button className="remove-btn" onClick={handleUserDelete} data-id={user.id}> <HiMinusCircle /> </button>
                         </div>
                         <button className="invite-btn"> 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z"/></svg>
