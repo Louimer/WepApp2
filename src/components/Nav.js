d2105d4660ea291be6caf6d0738d46260fd6fbc6
CLodberg <eaaclod@students.eaaa.dk>
2022-03-29 09:28:21 +0200

    Icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import homeIcon from "../assets/homeIcon.png";
+import createIcon from "../assets/createIcon.png";
+import profileIcon from "../assets/profileIcon.png";
 
 export default function Nav() {
   return (
@@ -7,9 +9,12 @@ export default function Nav() {
       <NavLink to="/">
         <img src={homeIcon} alt="Home Icon" />
       </NavLink>
-      <NavLink to="/create">Create</NavLink>
-      <NavLink to="/update">Update</NavLink>
-      <NavLink to="/profile">Profil</NavLink>
+      <NavLink to="/create">
+        <img src={createIcon} alt="Create Icon" />
+      </NavLink>
+      <NavLink to="/profile">
+        <img src={profileIcon} alt="Create Icon" />
+      </NavLink>
     </nav>
   );
 }
